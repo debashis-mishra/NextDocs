c6b05abd751764c3b88425fafe8ce31ca0965832
Debashis Mishra <debashis.007.m@gmail.com>
2024-08-16 01:18:09 +0530

    Added CollaborativeRoom component to document page
    This allows users to collaborate in real-time on the document.

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,53 @@
-import { Button } from '@/components/ui/button'
-import React from 'react'
+import Image from "next/image";
+import Header from "@/components/Header";
+import { Button } from "@/components/ui/button";
+import { SignedIn, UserButton } from "@clerk/nextjs";
+import React from "react";
+import AddDocumentBtn from "@/components/AddDocumentBtn";
+import { currentUser } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
-const Home = () => {
+const Home = async () => {
+  const clerkUser = await currentUser();
+  if (!clerkUser) {
+    redirect("/sign-in");
+  }
+
+  const documents = [];
+
   return (
     <div>
-      <Button>Click Me</Button>
-    </div>
-  )
-}
+      <main className="home-container">
+        <Header className="sticky left-0 top-0">
+          <div className="flex items-center gap-2 lg:gap-4">
+            Notification
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+          </div>
+        </Header>
 
-export default Home
+        {documents.length > 0 ? (
+          <div className=""></div>
+        ) : (
+          <div className="document-list-empty">
+            <Image
+              src="/assets/icons/doc.svg"
+              alt="Document"
+              width={40}
+              height={40}
+              className="mx-auto"
+            />
+
+            <AddDocumentBtn
+              userId={clerkUser.id}
+              email={clerkUser.emailAddresses[0].emailAddress}
+            />
+          </div>
+        )}
+      </main>
+    </div>
+  );
+};
+
+export default Home;
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,13 +1,14 @@
 import Link from "next/link";
 import Image from "next/image";
 import React from "react";
+import { cn } from "@/lib/utils";
 
-const Header = ({ children }: HeaderProps) => {
+const Header = ({ children, className }: HeaderProps) => {
   return (
-    <div className="header">
+    <div className={cn("header", className)}>
       <Link href="/" className="md:flex-1">
         <Image
-          src="/assets/icons/logo.svg"
+          src="/assets/icons/logo.png"
           alt={"NextDocs Logo"}
           width={120}
           height={32}
@@ -15,10 +16,10 @@ const Header = ({ children }: HeaderProps) => {
         />
 
         <Image
-          src="/assets/icons/logo-icon.svg"
+          src="/assets/icons/logo-icon.png"
           alt={"Logo"}
-          width={32}
-          height={32}
+          width={100}
+          height={100}
           className="mr-2 md:hidden"
         />
       </Link>
